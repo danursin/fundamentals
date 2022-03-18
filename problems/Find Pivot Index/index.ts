503b8605174871c4217f77eab4012a4cf8e8ee5a
danursin <danursin@gmail.com>
2022-03-18 18:19:55 -0500

    pivotIndex solution

--- problems/Find Pivot Index/index.ts
+++ problems/Find Pivot Index/index.ts
@@ -1,3 +1,17 @@
 export function pivotIndex(nums: number[]): number {
-    return -1;
+    const solution = -1;
+
+    for (let i = 0; i < nums.length; i += 1) {
+        const left = nums.slice(0, i);
+        const right = nums.slice(i + 1);
+
+        const leftSum = left.reduce((prev, cur) => prev + cur, 0);
+        const rightSum = right.reduce((prev, cur) => prev + cur, 0);
+
+        if (leftSum === rightSum) {
+            return i;
+        }
+    }
+
+    return solution;
 }
